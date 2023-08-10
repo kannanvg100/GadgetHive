09f50fa1ac3be1a98f6fb7179b713014f1370b5e
kannanvg100 <kannanx200@gmail.com>
2023-08-10 22:03:23 +0530

    server.js update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const os = require('os');
+const os = require('os')
 /* eslint-disable no-undef */
 require('dotenv').config()
 
@@ -11,36 +11,35 @@ db.init()
 const app = require('./app')
 
 //Handling Uncaught Exception
-process.on('uncaughtException', err => {
-    console.log(`Error:${err}`)
+process.on('uncaughtException', (err) => {
+	console.log(`Error:${err}`)
 	console.log(`Shutting down the server due to Uncaught Exception `)
 	process.exit(1)
 })
 
-const server = app.listen(process.env.PORT, () => {
-	const address = server.address();
-    const ipAddress = getIpAddress();
-    console.log(`Server is running at http://${ipAddress}:${address.port}`);
+const server = app.listen(process.env.PORT, '0.0.0.0', () => {
+	const address = server.address()
+	const ipAddress = getIpAddress()
+	console.log(`Server is running at http://${ipAddress}:${address.port}`)
 })
 
 function getIpAddress() {
-    const ifaces = os.networkInterfaces();
-    for (const iface in ifaces) {
-        for (const entry of ifaces[iface]) {
-            if (entry.family === 'IPv4' && !entry.internal) {
-                return entry.address;
-            }
-        }
-    }
-    return '127.0.0.1';
+	const ifaces = os.networkInterfaces()
+	for (const iface in ifaces) {
+		for (const entry of ifaces[iface]) {
+			if (entry.family === 'IPv4' && !entry.internal) {
+				return entry.address
+			}
+		}
+	}
+	return '127.0.0.1'
 }
 
 //Unhandled Promise Rejection
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
 	console.log(`Error:${err}`)
 	console.log(`Shutting down the server due to Unhandled Promise Rejection`)
 	server.close(() => {
 		process.exit(1)
 	})
 })
-
